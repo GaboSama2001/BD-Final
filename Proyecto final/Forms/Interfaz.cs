6e7df0995065d80faae8ac1f3f8bd4c985061075
Bryan josue portocarrero vega <Candidoportocarrero@gmail.com>
2024-11-25 20:03:43 -0600

    antes de empezar con la modificacion

--- Proyecto final/Forms/Interfaz.cs
+++ Proyecto final/Forms/Interfaz.cs
@@ -69,7 +69,7 @@ namespace Proyecto_final.Forms
 
         private void button3_Click(object sender, EventArgs e)
         {
-            AbrirFormEnPanel(new Facturas());
+            AbrirFormEnPanel(new Encargo());
         }
     }
 }
